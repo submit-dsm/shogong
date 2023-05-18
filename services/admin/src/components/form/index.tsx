f67fe2e7d0f4cb47b000cb16fc727011db5606b6
김진건 <87012465+jingeon27@users.noreply.github.com>
2023-05-18 13:45:35 +0900

    feat(admin):form구조 추가

---
+++ services/admin/src/components/form/index.tsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export interface SurveyProps {
+  type: string;
+}
+export const Survey = ({ type }: SurveyProps) => {
+  return (
+    <>
+      <_Layout>{}</_Layout>
+    </>
+  );
+};
+const _Layout = styled.div``;
