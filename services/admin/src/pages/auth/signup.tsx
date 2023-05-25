4b2b233ee13da084e271c4476646f7ee7d93b785
김진건 <87012465+jingeon27@users.noreply.github.com>
2023-05-26 00:28:53 +0900

    feat(admin):회원가입 페이지 퍼블리싱

--- services/admin/src/pages/auth/signup.tsx
+++ services/admin/src/pages/auth/signup.tsx
@@ -3,12 +3,21 @@ import { TextInput } from "@/components/input";
 import { Password } from "@/components/input/password";
 import { useAuth } from "@/hook/useAuth";
 import { AuthTemplate } from "@/components/auth";
-import { initialState, type InitialStateType } from "@/util/auth";
+import { InitialStateType, initialState } from "@/util/auth";
 const LoginPage: NextPage = () => {
-  const { state, onInput } = useAuth<InitialStateType>(initialState);
+  const { state, onInput } = useAuth<InitialStateType & { name: string }>({
+    ...initialState,
+    name: "",
+  });
   return (
     <>
       <AuthTemplate legend="로그인" onSubmit={() => {}}>
+        <TextInput
+          {...{ onInput }}
+          name={"name"}
+          label={"이름"}
+          value={state.account_id}
+        />
         <TextInput
           {...{ onInput }}
           name={"account_id"}
