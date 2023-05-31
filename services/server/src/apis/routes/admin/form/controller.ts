8f55d90229c4ce48bfd99796fa7c15f101ecd654
Josanghyeon <05lazy.dev@gmail.com>
2023-05-31 13:49:00 +0900

    학생 검색

--- services/server/src/apis/routes/admin/form/controller.ts
+++ services/server/src/apis/routes/admin/form/controller.ts
@@ -2,6 +2,9 @@ import { getAccessToken } from "@/utils/jwt";
 import { NextFunction, Request, Response } from "express";
 import FormService from "./service";
 import { JWTHelper } from "@/helpers/jwt";
+import axios from "axios";
+import { instance } from "@/utils/axios";
+
 export const handleGetForm = async (
   _req: Request,
   res: Response,
@@ -46,4 +49,17 @@ export const handleFormDelete = (
 ) => {
   try {
   } catch (e) {}
+};
+
+export const handleGetAllStudent = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const response = await instance.get("/users/role?roleName=STU");
+    res.status(200).send(response.data);
+  } catch (e) {
+    next(e);
+  }
 };
--- services/server/src/apis/routes/admin/form/index.ts
+++ services/server/src/apis/routes/admin/form/index.ts
@@ -1,7 +1,12 @@
 import validateToken from "@/apis/middlewares/validate-token";
 import { Router } from "express";
-import { handleCreateForm, handleGetForm } from "./controller";
+import {
+  handleCreateForm,
+  handleGetAllStudent,
+  handleGetForm,
+} from "./controller";
 export const form = Router();
 
 form.get("/", validateToken, handleGetForm);
 form.post("/", validateToken, handleCreateForm);
+form.get("/student", validateToken, handleGetAllStudent);
